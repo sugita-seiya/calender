10a97f3bae963706f74d88113a751c9a8bd69ac7
sugita-seiya <satreu16@ezweb.ne.jp>
2020-06-28 07:02:01 +0900

    カレンダー機能のマークアップ実装

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/holiday','CalendarController@getHoliday');           // 休日データの取得
+Route::post('/holiday','CalendarController@postHoliday');         //POSTで受信した休日データの登録
+Route::get('/','CalendarController@index');                       //カレンダー表の表示
+Route::get('/holiday/{id}','CalendarController@getHolidayId');
+Route::delete('/holiday','CalendarController@deleteHoliday');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('calendar.holiday');
+// });
