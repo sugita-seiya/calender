807cffe4bd446f99f5a0dc1661659b33fe302229
sugita-seiya <satreu16@ezweb.ne.jp>
2020-07-05 06:04:10 +0900

    マークアップの修正

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,8 +10,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
-    @yield('content')
-</div>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
